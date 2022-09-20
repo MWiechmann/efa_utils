0033c90b4b8e73cb0e8f61432a9dbb0fb3d8140a
Marcel Wiechmann <mail@mwiechmann.com>
2022-09-20 12:40:22 +0200

    update documentation

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='efa_utils',
     packages=find_packages(include=['efa_utils']),
-    version='0.1',
-    description='Custom utility functions for exploratory factor analysis',
+    version='0.2',
+    description='Custom utility functions for exploratory factor analysis with the factor_analyzer package.',
     author='Dr. Marcel Wiechmann',
     license='GPLv3',
     install_requires=['numpy', 'pandas', 'factor_analyzer'],
